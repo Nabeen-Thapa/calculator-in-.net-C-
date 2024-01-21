7cbbd112ebb238c303722af0ea99970ef3da3803
Nabin Thapa <151662784+Nabeen-Thapa@users.noreply.github.com>
2024-01-21 22:19:56 +0545

    Add files via upload

---
+++ Calculator_aspx.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator_aspx.aspx.cs" Inherits="Calculator_Asp.Calculator_aspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <link href="Calculatoraspx_css.css" rel="stylesheet" />
+    <style>
+        body {
+    margin:0px;
+    padding: 0px;
+    align-content:center;
+}
+
+#calculator {
+    padding:20px;
+    background-color:deepskyblue;
+    height:220px;
+    width:15%;
+    margin-left:300px;
+}
+#firstnum{
+    margin-top:10px;
+   background-color:white;
+    Border:1px solid blue;
+    width:80%;   
+}
+#secondnum {
+    margin-top: 30px;
+    background-color: white;
+    Border: 1px solid blue;
+    width: 80%;
+   margin-bottom:10px;
+}
+#idoutput1 {
+    margin-top:20px;
+    margin-bottom:20px;
+    display:block;
+    
+}
+#idbutton, #idbutton1 {
+    display: block;
+    margin-top:10px;
+}
+#idadd, #idsub, #iddiv, #idmul
+{
+   
+    width:90px;  
+    margin:5px;
+}
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div runat="server" id="calculator">
+           <asp:TextBox runat="server" id="firstnum"/>
+           <asp:TextBox runat="server" id="secondnum"/>
+                <span runat="server" id="idoutput1"></span> 
+           <div runat="server" id="idbutton">
+               <asp:Button Text="Add" runat="server" id="idadd" OnClick="add_click"/>
+                <asp:Button Text="Subtract" runat="server" id="idsub" OnClick="sub_click"/>
+            </div>
+            <div runat="server" id="idbutton1">
+               <asp:Button Text="Divide" runat="server" id="iddiv" OnClick="div_click"/>
+               <asp:Button Text="Multiply" runat="server" id="idmul" OnClick="mul_click"/>
+           </div><br />
+           
+       </div>
+    </form>
+    
+</body>
+</html>
---
+++ Calculator_aspx.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator_Asp
+{
+    public partial class Calculator_aspx : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void add_click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(firstnum.Text) && (!String.IsNullOrEmpty(secondnum.Text)))
+            {
+                String input1 = firstnum.Text;
+                String input2 = secondnum.Text;
+                int value1, value2;
+                if (int.TryParse(input1 , out value1) && int.TryParse(input2, out value2))
+                {
+                    int result = value1 + value2;
+                    idoutput1.InnerHtml ="addition : " + result.ToString();   
+                }
+                else
+                {
+                    idoutput1.InnerHtml = "enter the integer for calculation";
+                }
+                
+            } else{
+                idoutput1.InnerHtml = "enter the integer for calculation";
+
+            }
+        }
+        protected void sub_click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(firstnum.Text) && (!String.IsNullOrEmpty(secondnum.Text)))
+            {
+                String input1 = firstnum.Text;
+                String input2 = secondnum.Text;
+                int value1, value2;
+                if (int.TryParse(input1, out value1) && int.TryParse(input2, out value2))
+                {
+                    int result = value1 - value2;
+                    idoutput1.InnerHtml = "subtraction : " + result.ToString();
+                }
+                else
+                {
+                    idoutput1.InnerHtml = "enter the integer for calculation";
+                }
+
+            }
+            else
+            {
+                idoutput1.InnerHtml = "enter the integer for calculation";
+
+            }
+        }
+        protected void div_click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(firstnum.Text) && (!String.IsNullOrEmpty(secondnum.Text)))
+            {
+                String input1 = firstnum.Text;
+                String input2 = secondnum.Text;
+                int value1, value2;
+                if (int.TryParse(input1, out value1) && int.TryParse(input2, out value2))
+                {
+                    int result = value1 / value2;
+                    idoutput1.InnerHtml = "division : " + result.ToString();
+                }
+                else
+                {
+                    idoutput1.InnerHtml = "enter the integer for calculation";
+                }
+
+            }
+            else
+            {
+                idoutput1.InnerHtml = "enter the integer for calculation";
+
+            }
+        }
+        protected void mul_click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(firstnum.Text) && (!String.IsNullOrEmpty(secondnum.Text)))
+            {
+                String input1 = firstnum.Text;
+                String input2 = secondnum.Text;
+                int value1, value2;
+                if (int.TryParse(input1, out value1) && int.TryParse(input2, out value2))
+                {
+                    int result = value1 * value2;
+                    idoutput1.InnerHtml = "multiplication : " + result.ToString();
+                }
+                else
+                {
+                    idoutput1.InnerHtml = "enter the integer for calculation";
+                }
+
+            }
+            else
+            {
+                idoutput1.InnerHtml = "enter the integer for calculation";
+
+            }
+        }
+    }
+}
